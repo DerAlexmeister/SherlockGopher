73fa812f7e1f13e2bb7d101d1ddf973904e4bc5d
DerAlexx <alexweb199@hotmail.de>
2020-03-22 22:07:07 +0100

    Added first to restmethods to the restendpoint

--- webserver/main.go
+++ webserver/main.go
@@ -1,10 +1,54 @@
 package main
 
-const (
-	servicename string = "Reactservice"
-	address     string = "*:8081"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/web"
+	webserver "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/webserver/webserver"
 )
 
+const (
+	servicename string = "CrawlWebServer"
+	address     string = "localhost:8081"
+)
+
+/*
+getServiceName will return a the service name of this service.
+*/
+func getServiceName() string {
+	return servicename
+}
+
+/*
+getAddress will return the address of the service.
+*/
+func getAddress() string {
+	return address
+}
+
 func main() {
+	service := web.NewService(web.Name(getServiceName()))
+
+	err := service.Init()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	webServerService := webserver.New()
+
+	router := gin.Default()
+
+	router.GET("/helloping", webServerService.Helloping)
+	router.POST("search", webServerService.RecieveURL)
+
+	router.Run(getAddress())
+
+	service.Handle("/", router)
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 
 }
--- webserver/webserver/webserver.go
+++ webserver/webserver/webserver.go
@@ -1,1 +1,56 @@
 package webserver
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+/*
+CrawlWebServer will be the webserver being the man in the middle between the frontend and the backend.
+*/
+type CrawlWebServer struct {
+}
+
+/*
+RequestedURL will be the struct for the post request of the domain to crawl.
+*/
+type RequestedURL struct {
+	URL string `json:"url" binding:"required"`
+}
+
+/*
+NewRequestedURL will be a new instance of RequestedURL.
+*/
+func NewRequestedURL() *RequestedURL {
+	return &RequestedURL{}
+}
+
+/*
+New will return a new instance of the CrawlWebServer
+*/
+func New() *CrawlWebServer {
+	return &CrawlWebServer{}
+}
+
+/*
+Helloping Ping will return just for testing purposes a pong. Like PING PONG.
+*/
+func (server *CrawlWebServer) Helloping(context *gin.Context) {
+	context.JSON(200, map[string]string{
+		"message": "Pong",
+	})
+}
+
+/*
+RecieveURL will handle the requested url which should be crawled.
+*/
+func (server *CrawlWebServer) RecieveURL(context *gin.Context) {
+	var url = NewRequestedURL()
+	context.BindJSON(url)
+	context.JSON(http.StatusOK, gin.H{
+		"status": "Fine",
+	})
+	fmt.Println(url) //TODO check if url is empty
+}
