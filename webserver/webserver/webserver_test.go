58d550f3fb46ec61acb9434f2f4be4dac5a5e06f
DerAlexx <alexweb199@hotmail.de>
2020-03-23 14:14:59 +0100

    Added some Test

--- webserver/webserver/webserver_test.go
+++ webserver/webserver/webserver_test.go
@@ -1,1 +1,28 @@
 package webserver
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestSendHelloPing(t *testing.T) { // TODO drüber schauen.
+
+	req, err := http.NewRequest("GET", "localhost:8080/helloping", nil)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	log.Printf("Body %s", req.Body)
+	t.Log(req)
+}
+
+func TestSubmitURL(t *testing.T) {
+	req, err := http.NewRequest("POST", "/search", nil)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println(req)
+}
