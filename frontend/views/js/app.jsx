b087aeee70f589f3477fcc01fd308b02e2c4cc30
DerAlexx <alexweb199@hotmail.de>
2020-03-29 20:31:38 +0200

    Fixed Search issue, now the frontend should be able to submit a url to the backend service

--- frontend/views/js/app.jsx
+++ frontend/views/js/app.jsx
@@ -1,27 +1,46 @@
 //React app
-import axios from 'axios';
-
 const RESTENDPOINT = "localhost:8081"
+const SEARCHENDPOINT = RESTENDPOINT + "/search/"
 
+// Just the Searchbar Component with the logo, title and the searchbar to submit a url.
 class SearchBar extends React.Component {
 
+  state = {
+    value: ""
+  }
+
+  // CTor for the Searchbar Component.
   constructor(props) {
     super(props);
-    this.state = { url: '' };
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({
-      url: event.target.url
-    })
-    console.log(this.state.url)
+  // Making the POST REQUEST
+  serverRequest(url) {
+    $.post(
+      SEARCHENDPOINT,
+      {
+        url: this.state.value
+      },
+      response => {
+        // TODO
+      }
+    );
   }
 
+  // Handling the Change of the Inputfield. 
+  handleChange(event) {    
+    this.setState(
+      { 
+        value: event.target.value
+      }
+    );  
+  }
+
+  // Handle the submit of the Form.
   handleSubmit(event) {
-    alert("This will be sent of " + this.state.url)
+    alert('A Website was submitted: ' + this.state.value);
   }
 
   render() {
@@ -30,7 +49,7 @@ class SearchBar extends React.Component {
         <img class="logo" alt="sherlock gopher" src="img/sherlockgopher.png"></img>
         <h4 class="title">Sherlock Gopher</h4>
         <form onSubmit={this.handleSubmit} class="searchbarform" method="POST">
-          <input class="searchbarinput" value={this.state.url} onChange={this.handleChange} name="url" placeholder="Hier könnte Ihre Werbung stehen..."></input>
+          <input class="searchbarinput" value={this.state.value} onChange={this.handleChange} name="url" placeholder="Hier könnte Ihre Werbung stehen..."></input>
           <button class="searchbarbutton" type="submit">Schnüffel</button>
         </form>
       </div>
@@ -38,6 +57,7 @@ class SearchBar extends React.Component {
   }
 }
 
+// The actual APP Component which will be rendered by calling the Website.
 class App extends React.Component {
     render() {
       return (
