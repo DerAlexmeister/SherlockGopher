d1890ec82fdfed81b33834052be5dd8c6698e76c
schroedp <pschroe08932@googlemail.com>
2020-03-29 18:51:41 +0200

    crawler send http to analyzer

--- crawler/crawler/crawler.go
+++ crawler/crawler/crawler.go
@@ -1,2 +1,31 @@
 package crawler
 
+import (
+	"github.com/golang/protobuf/ptypes/empty"
+	proto "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/crawler/proto"
+)
+
+type CrawlerServiceHandler struct{}
+
+const chunkSize = 64 * 1024 // 64 KiB
+
+type chunkerSrv []byte
+
+func NewCrawlerServiceHandler() *CrawlerServiceHandler {
+	return &CrawlerServiceHandler{}
+}
+
+func (c chunkerSrv) SendHttpPage(_ *empty.Empty, srv proto.Chunker_ChunkerStream) error {
+	chnk := &proto.Chunk{}
+	for currentByte := 0; currentByte < len(c); currentByte += chunkSize {
+		if currentByte+chunkSize > len(c) {
+			chnk.Chunk = c[currentByte:len(c)]
+		} else {
+			chnk.Chunk = c[currentByte : currentByte+chunkSize]
+		}
+		if err := srv.Send(chnk); err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- crawler/main.go
+++ crawler/main.go
@@ -1,7 +1,24 @@
 package main
 
-// Crawler file
+import (
+	"github.com/micro/go-micro"
+	proto "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/crawler/proto"
+	impl "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/crawler/crawler"
+)
+
+const serviceName = "crawler-service"
 
 func main(){
+
+	service := micro.NewService(
+		micro.Name(serviceName),
+	)
+
+	err := proto.RegisterCrawlerHandler(service.Server(), impl.NewCrawlerService())
+
+	service.Init()
+
+	if err := service.Run(); err != nil {
+		log.Fatalf("Failed to start service. Error:\n\t%s", err.Error())
 	
 }
