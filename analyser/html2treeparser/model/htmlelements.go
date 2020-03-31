5f5b00f308a5390d423b8d3f4ccf7a63121df135
Markus Wiesheu <wiesheu@hm.edu>
2020-03-31 13:26:20 +0200

    Schnittstelle Parser-Analyzer hinzugefügt

---
+++ analyser/html2treeparser/model/htmlelements.go
@@ -0,0 +1,14 @@
+package model
+
+type Tag struct {
+
+}
+
+type TagType string
+type TagContent string
+type AttributeType string
+
+type TagAttribute struct {
+	attrType AttributeType
+}
+
---
+++ analyser/html2treeparser/model/tree.go
@@ -0,0 +1,9 @@
+package model
+
+type HTMLTree struct {
+
+}
+
+type Node struct {
+
+}
