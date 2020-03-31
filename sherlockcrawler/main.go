cf2e6fc1bba0a71e985a95aa6e1c4ff01f4244b9
DerAlexx <alexweb199@hotmail.de>
2020-03-31 11:53:08 +0200

    Fixed some errors in crawler main

--- sherlockcrawler/main.go
+++ sherlockcrawler/main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/micro/go-micro"
-	proto "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/crawler/proto"
-	impl "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/crawler/crawler"
 )
 
 const serviceName = "crawler-service"
 
-func main(){
+func main() {
 
-	service := micro.NewService(
-		micro.Name(serviceName),
-	)
-
-	err := proto.RegisterCrawlerHandler(service.Server(), impl.NewCrawlerService())
-
+	service := micro.NewService(micro.Name(serviceName))
 	service.Init()
 
 	if err := service.Run(); err != nil {
 		log.Fatalf("Failed to start service. Error:\n\t%s", err.Error())
-	
+	}
 }
