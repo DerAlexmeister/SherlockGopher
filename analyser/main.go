310108a5f9e063f46fd66cbe57b08f74a971607e
Markus Wiesheu <wiesheu@hm.edu>
2020-03-31 13:30:03 +0200

    Schnittstelle Parser-Analyzer hinzugefügt

--- analyser/main.go
+++ analyser/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/analyser/html2treeparser/model"
+)
+
 func main() {
-	
+	fmt.Println("test")
+	rootNode := model.NewHTMLTree("<html></html>").Parse()
 }
