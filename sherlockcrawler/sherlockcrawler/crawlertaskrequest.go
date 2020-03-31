2178fbfd14147cc3596e4992745a86caffc08edd
DerAlexx <alexweb199@hotmail.de>
2020-03-31 13:19:08 +0200

    Rearranged the structure of the sherlockcrawler package

---
+++ sherlockcrawler/sherlockcrawler/crawlertaskrequest.go
@@ -0,0 +1,33 @@
+package sherlockcrawler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+/*
+CrawlerTaskRequest will be a request made by the analyser.
+*/
+type CrawlerTaskRequest struct {
+	addr     string
+	isDone   bool
+	response *http.Response
+}
+
+/*
+MakeRequestForHTML will make a request to a given Website and return its HTML-Code.
+*/
+func (creq *CrawlerTaskRequest) MakeRequestForHTML() (*http.Response, error) {
+	response, err := http.Get(creq.addr)
+	if err != nil {
+		return nil, fmt.Errorf("An error occured while trying to get the Website: %s", creq.addr)
+	}
+	return response, nil
+}
+
+/*
+SetResponse will set the response of the Request to a given CrawlerTaskRequest.
+*/
+func (creq *CrawlerTaskRequest) SetResponse(lresponse *http.Response) error {
+	return nil
+}
