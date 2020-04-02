f8bc327914a5500540abc25d0988cb9fd4dec74b
DerAlexx <alexweb199@hotmail.de>
2020-04-02 21:57:57 +0200

    Added Testingfile for neo4j recompiled protofile and changed some smaller things

--- analyser/main.go
+++ analyser/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"github.com/micro/go-micro"
-	proto "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/analyser/proto"
-	impl "github.com/ob-algdatii-20ss/leistungsnachweis-dievierausrufezeichen/analyser/analyser"
 )
 
 const serviceName = "analyser-service"
 
-func main(){
+func main() {
 
 	service := micro.NewService(
 		micro.Name(serviceName),
 	)
 
-	err := proto.RegisterAnalyserHandler(service.Server(), impl.NewAnalyserService())
+	//err := proto.AnalyserService()
 
 	service.Init()
 
-	if err := service.Run(); err != nil {
-		log.Fatalf("Failed to start service. Error:\n\t%s", err.Error())
-	
+	/*
+		if err := service.Run(); err != nil {
+			log.Fatalf("Failed to start service. Error:\n\t%s", err.Error())
+		}
+	*/
 }
