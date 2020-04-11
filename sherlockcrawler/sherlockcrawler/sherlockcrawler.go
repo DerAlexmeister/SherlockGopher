2d5e62764fd3af05686eb3a593c766cb338bf738
DerAlexx <alexweb199@hotmail.de>
2020-04-11 17:14:02 +0200

    Added some stuff in the sherlockcrawler main file

--- sherlockcrawler/sherlockcrawler/sherlockcrawler.go
+++ sherlockcrawler/sherlockcrawler/sherlockcrawler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	proto "github.com/ob-algdatii-20ss/SherlockGopher/sherlockcrawler/proto/crawlertoanalyser"
+	"github.com/pkg/errors"
 )
 
 /*
@@ -23,8 +24,21 @@ func (sherlock *Sherlockcrawler) InjectDependency(deps *Sherlockdependencies) {
 }
 
 /*
+getQueue will return a pointer to the queue.
+*/
+func (sherlock *Sherlockcrawler) getQueue() *CrawlerQueue {
+	return &sherlock.Queue
+}
+
+/*
 CreateTask will append the current queue with a task.
 */
 func (sherlock *Sherlockcrawler) CreateTask(ctx context.Context, in *proto.CrawlTaskCreateRequest, out *proto.CrawlTaskCreateResponse) error {
-	return nil
+	task := NewTask()
+	task.setAddr(in.GetUrl())
+	if id := (*sherlock.getQueue()).AppendQueue(&task); id > 0 {
+		return nil
+	}
+	out.Statuscode = proto.URL_STATUS_failure //TODO
+	return errors.New("cannot create a task for this queue")
 }
