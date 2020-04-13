bb44fc475a38ab3e1d1db423aec41e751044d0e5
DerAlexx <alexweb199@hotmail.de>
2020-04-13 20:54:36 +0200

    Fixed issue with package inaccessibility

--- analyser/sherlockanalyser/analyserdependencies.go
+++ analyser/sherlockanalyser/analyserdependencies.go
@@ -1,20 +1,18 @@
 package sherlockanalyser
 
-import crawlerproto "github.com/ob-algdatii-20ss/SherlockGopher/sherlockcrawler/proto/crawlertoanalyser"
-
 /*
 AnalyserDependency represents all services which are needed to run the analyser.
 */
 type AnalyserDependency struct {
 	Neo4J   func() //TODO
-	Crawler func() crawlerproto.AnalyserInterface
+	Crawler func() //crawlerproto.AnalyserInterface
 }
 
 /*
 NewAnalyserDependencies will return an empty dependency struct.
 */
-func NewAnalyserDependencies(crawler crawlerproto.AnalyserInterface) *AnalyserDependency {
+func NewAnalyserDependencies() *AnalyserDependency {
 	return &AnalyserDependency{
-		Crawler: crawler
+		//Crawler: crawler
 	}
 }
