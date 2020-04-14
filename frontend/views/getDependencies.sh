06568155a9ad186ce1ffa0db1e2276b9995e933c
DerAlexx <alexweb199@hotmail.de>
2020-04-14 15:49:30 +0200

    Removed unneeded javascritpfiles

--- frontend/views/getDependencies.sh
+++ frontend/views/getDependencies.sh
@@ -5,7 +5,7 @@ mkdir -p staticdeps
 cd staticdeps
 
 echo "[+] Downloading all JavaScript Dependencies."
-curl http://code.jquery.com/jquery-2.1.4.min.js --output jquery-2.1.4.min.js
+#curl http://code.jquery.com/jquery-2.1.4.min.js --output jquery-2.1.4.min.js
 curl https://unpkg.com/react@16.0.0/umd/react.production.min.js --output react.production.min.js
 curl https://unpkg.com/react-dom@16.0.0/umd/react-dom.production.min.js --output react-dom.production.min.js
 curl https://unpkg.com/babel-standalone@6.26.0/babel.js --output babel.js
