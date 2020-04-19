c66eed7aab86e2277e68f2132da374e49bc22164
Johannes Walter <jwalter@hm.edu>
2020-04-19 18:24:28 +0200

    #44: AnalyserQueue
    Implementation and Test

--- analyser/sherlockanalyser/analyserqueue_test.go
+++ analyser/sherlockanalyser/analyserqueue_test.go
@@ -1,75 +1,182 @@
 package sherlockanalyser
 
 import (
-	"strconv"
 	"testing"
 )
 
-func TestNewAnalyserQueue(t *testing.T) {
-	que := getTestQueue()
-
-	if que.IsEmpty() {
-		t.Error("que failed")
-	}
-}
-
-func TestContainsAddress(t *testing.T) {
-	que := getTestQueue()
-
-	if _, ok, err := que.ContainsAddress("www.1234.de"); err != nil && !ok {
-		t.Error("element is missing")
-	}
-}
-
-func TestContainsID(t *testing.T) {
-	que := getTestQueue()
-
-	if id, ok, err := que.ContainsAddress("www.1234.de"); err == nil && ok {
-		if !que.ContainsID(id) {
-			t.Error("element is missing")
+/*
+containsElement will check whether a given slice has an element or not.
+*/
+func containsElement(s []uint64, id uint64) bool {
+	for _, i := range s {
+		if i == id {
+			return true
 		}
+	}
+	return false
+}
+
+//=======================================================================
+// TESTS
+//=======================================================================
+
+/*
+TestQueueHasIDAlreadyInUse tests whether a id is already in use.
+*/
+func TestQueueHasIDAlreadyInUse(t *testing.T) {
+	queue := NewAnalyserQueue()
+	task := &analyserTaskRequest{}
+	id := queue.AppendQueue(task)
+	if id == 0 {
+		t.Fatal("got a 0 id from the AppendQueue-Method")
+	} else if contains := queue.ContainsTaskID(id); !contains {
+		t.Fatalf("id is not in the queue altought it should be. ID: %d, Contains: %t", id, contains)
 	} else {
-		t.Error("element is missing")
+		t.Log("successfully found ID in task queue")
+	}
+
+}
+
+/*
+TestQueueContainsTaskIDFailes tests trys
+*/
+func TestQueueContainsTaskIDFailes(t *testing.T) {
+	queue := NewAnalyserQueue()
+	task := &analyserTaskRequest{}
+	id := queue.AppendQueue(task)
+	if id == 0 {
+		t.Fatal("got a 0 id from the AppendQueue-Method")
+	} else if contains := queue.ContainsTaskID(id + 1); contains {
+		t.Fatalf("id is not in the queue altought it should be. ID: %d, Contains: %t", id, contains)
+	} else {
+		t.Log("successfully found ID in taskqueue")
+	}
+
+}
+
+/*
+TestQueueAppendFailesBecauseOfNilPointer will try to make the Apppend Method fail because of a nullpointer.
+*/
+func TestQueueAppendFailesBecauseOfNilPointer(t *testing.T) {
+	queue := NewAnalyserQueue()
+	id := queue.AppendQueue(nil)
+	if id == 0 {
+		t.Log("got a zero returncode as expected", 0, id)
+	} else {
+		t.Fatalf("got a non-zero returncode. Expected: %d, Got: %d", 0, id)
+	}
+
+}
+
+/*
+TestQueueRemoveTask will append a task to the queue and remove it.
+*/
+func TestQueueRemoveTask(t *testing.T) {
+	queue := NewAnalyserQueue()
+	task := &analyserTaskRequest{}
+	id := queue.AppendQueue(task)
+	if id == 0 {
+		t.Fatal("go a 0 id from the AppendQueue-Method")
+	} else if contains := queue.ContainsTaskID(id); !contains {
+		t.Fatalf("id is not in the queue altought it should be. ID: %d, Contains: %t", id, contains)
+	} else {
+		t.Log("successfully added task to the taskqueue")
+	}
+	if works := queue.RemoveFromQueue(id); !works {
+		t.Fatalf("couldnt remove task with valid id Expected: %t, Got: %t, ID: %d", true, works, id)
+	} else {
+		t.Log("successfully removed task")
 	}
 }
 
-func TestAppendQueue(t *testing.T) {
-	que := getTestQueue()
-
-	task := NewTask(getTestData(9999999))
-	if !que.AppendQueue(&task) {
-		t.Error("append alement failed")
+/*
+TestQueueRemoveTaskFailedBecauseOfZeroID will fail because of a zero id.
+*/
+func TestQueueRemoveTaskFailedBecauseOfZeroID(t *testing.T) {
+	queue := NewAnalyserQueue()
+	task := &analyserTaskRequest{}
+	id := queue.AppendQueue(task)
+	if id == 0 {
+		t.Fatal("go a 0 id from the AppendQueue-Method")
+	} else if contains := queue.ContainsTaskID(id); !contains {
+		t.Fatalf("id is not in the queue altought it should be. ID: %d, Contains: %t", id, contains)
+	} else {
+		t.Log("successfully added task to the taskqueue")
+	}
+	if works := queue.RemoveFromQueue(0); works {
+		t.Fatalf("could remove task with unvalid id Expected: %t, Got: %t, ID: %d", false, works, id)
+	} else {
+		t.Log("successfully removed task")
 	}
 }
 
-func TestRemoveFromQueue(t *testing.T) {
-	que := getTestQueue()
+/*
+TestGetQueueStatus will test the status of the queue and show its status.
+*/
+func TestGetQueueStatus(t *testing.T) {
+	message := "States of the Task currently in the Queue. \nUndone: 1, Processing: 1, Finished: 1, Failed: 1"
+	queue := NewAnalyserQueue()
+	task1 := &analyserTaskRequest{}
+	task2 := &analyserTaskRequest{}
+	task3 := &analyserTaskRequest{}
+	task4 := &analyserTaskRequest{}
+	task5 := &analyserTaskRequest{}
+	task6 := &analyserTaskRequest{}
 
-	if !que.RemoveFromQueue(1234) {
-		t.Error("remove alement failed")
+	task1.SetState(UNDONE)
+	task2.SetState(PROCESSING)
+	task3.SetState(CRAWLERERROR)
+	task4.SetState(SAVING)
+	task5.SetState(SENDTOCRAWLER)
+	task6.SetState(FINISHED)
+
+	id1 := queue.AppendQueue(task1)
+	id2 := queue.AppendQueue(task2)
+	id3 := queue.AppendQueue(task3)
+	id4 := queue.AppendQueue(task4)
+	id5 := queue.AppendQueue(task5)
+	id6 := queue.AppendQueue(task6)
+
+	if id1 == 0 || id2 == 0 || id3 == 0 || id4 == 0 || id5 == 0 || id6 == 0{
+		t.Fatalf("Missmatching ids excpeted non-zero value but got id1: %d, id2: %d, id3: %d, id4: %d, id5: %d, id6: %d", id1, id2, id3, id4, id5, id6)
+	} else {
+		t.Log("successfully creates 4 different tasks with different states")
+	}
+
+	status := queue.GetStatusOfQueue()
+
+	if status != message {
+		t.Fatalf("messages does not match Expected: %s, Got: %s", message, status)
+	} else {
+		t.Log("message go as expected.")
 	}
 }
 
-func getTestQueue() AnalyserQueue {
-	aq := NewAnalyserQueue()
+/*
+TestGetAllTaskIds if GetAllTaskIds will return all ids.
+*/
+func TestGetAllTaskIds(t *testing.T) {
+	queue := NewAnalyserQueue()
+	task1 := &analyserTaskRequest{}
+	task2 := &analyserTaskRequest{}
+	id1 := queue.AppendQueue(task1)
+	id2 := queue.AppendQueue(task2)
 
-	var i uint64
-	for i = 0; i < 10000; i++ {
-		task := NewTask(getTestData(i))
-		aq.AppendQueue(&task)
+	if id1 == 0 || id2 == 0 {
+		t.Fatalf("Missmatching ids excpeted non-zero value but got id1: %d, id2: %d", id1, id2)
+	} else {
+		t.Log("successfully creates 4 different tasks with different states")
 	}
 
-	return aq
-}
+	ids := queue.getAllTaskIds()
 
-func getTestData(id uint64) CrawlerData {
-	return CrawlerData{
-		taskId:            id,
-		addr:              "www." + strconv.FormatUint(id, 10) + ".de",
-		taskError:         nil,
-		responseHeader:    nil,
-		responseBodyBytes: []byte("html"),
-		statusCode:        200,
-		responseTime:      0,
+	if length := len(ids); length != 2 {
+		t.Fatalf("The length of the slice of ids does not be match the real one. Expected: %d, Got: %d", 2, length)
+	} else if contains := containsElement(ids, id1); !contains {
+		t.Fatalf("element in should be in map but wasnt there. Coulndt find: %d", id1)
+	} else if contains := containsElement(ids, id2); !contains {
+		t.Fatalf("element in should be in map but wasnt there. Coulndt find: %d", id2)
+	} else {
+		t.Log("successfully found all elements")
 	}
 }
